e443bb4c6be58d1a06c274e4cc4da6a3995efffa
Andrey Tuktam <tuktam@mail.ru>
2022-06-27 22:57:28 +0700

    Z2

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB){
+    Console.Write("max=");
+    Console.Write(numberA);
+} else{
+    Console.Write("max=");
+    Console.Write(numberB);
+}
