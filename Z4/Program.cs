2b7d168b01eb9a9272466af846a57ee0094d78e8
Andrey Tuktam <tuktam@mail.ru>
2022-06-28 07:03:41 +0700

    Z4

---
+++ Z4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число C: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB && numberA > numberC ){
+    Console.Write("max=");
+    Console.Write(numberA);
+}if (numberB > numberA && numberB > numberC ){
+    Console.Write("max=");
+    Console.Write(numberB);
+}if (numberC > numberA && numberC > numberB ){
+    Console.Write("max=");
+    Console.Write(numberC);
+}
+
