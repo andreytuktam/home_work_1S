33994e77fd0448efbe62f94eadc04c39d437e843
Andrey Tuktam <tuktam@mail.ru>
2022-07-01 22:21:52 +0700

    Z6

---
+++ Z6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+    if (numberA % 2 == 0)  {
+    Console.Write(numberA);
+    Console.Write("- четное число");
+    } else{
+    Console.Write(numberA);    
+    Console.Write("- не четное число");
+    }
+
