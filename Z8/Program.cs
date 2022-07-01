6149bf66080ff30339b73241e4894e202b29b12f
Andrey Tuktam <tuktam@mail.ru>
2022-07-02 01:09:50 +0700

    Z8

---
+++ Z8/Program.cs
@@ -0,0 +1,14 @@
+﻿//Console.WriteLine("Введите число A: ");
+//int numberA = Convert.ToInt32(Console.ReadLine());
+int N = 0;
+int i = 0;
+int F = N + i;
+while (i <= 9);
+    if ((N + i) % 2 == 0) {
+        //chet.Concat(new int[] { (N + i) }).ToArray();
+        Console.WriteLine(F); 
+        i = i + 1;
+    } else {
+        i = i + 1;
+        }
+
