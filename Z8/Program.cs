18fedf68ebd30ae32212fbe36f3ce55182bc6eba
Andrey Tuktam <tuktam@mail.ru>
2022-07-02 22:25:01 +0700

    Z8

--- Z8/Program.cs
+++ Z8/Program.cs
@@ -1,14 +1,9 @@
-﻿//Console.WriteLine("Введите число A: ");
-//int numberA = Convert.ToInt32(Console.ReadLine());
-int N = 0;
+﻿Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
 int i = 0;
-int F = N + i;
-while (i <= 9);
-    if ((N + i) % 2 == 0) {
-        //chet.Concat(new int[] { (N + i) }).ToArray();
-        Console.WriteLine(F); 
-        i = i + 1;
-    } else {
-        i = i + 1;
-        }
-
+while (i <= numberA);
+    if (i % 2 == 0) 
+    {
+        Console.Write(i); 
+    } 
+    i++;
